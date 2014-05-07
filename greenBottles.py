2b7e595eecff71114bf617e13c9643646344308e
GaddafiNinja <jaykin7072@sohamcollege.org.uk>
2014-05-07 09:41:25 +0100

    Update greenBottles.py
    such swags

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,9 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+bottles = 10
+for x in range(10):
+    print("{0} green bottles, hanging on the wall".format(bottles))
+    print("{0} green bottles, hanging on the wall".format(bottles))
+    print("And if 1 green bottle should acidentally fall,")
+    print("There'll be {0} green bottles hanging on the wall.".format(bottles-1))
+    bottles-=1
+    time.sleep(1)
